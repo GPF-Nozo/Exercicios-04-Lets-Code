3adde95495733fb20953d245cffd5c27b78941a2
GPF-Nozo <90787054+GPF-Nozo@users.noreply.github.com>
2021-09-22 16:14:55 -0300

    Update QuestaoSete.java

--- QuestaoSete.java
+++ QuestaoSete.java
@@ -3,17 +3,33 @@ package br.com.letscode.java;
 import java.util.Scanner;
 
 class Pessoa {
-    public String nome;
-    public int idade;
+    private String nome;
+    private int idade;
     public Pessoa() {
+
     }
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
 
-public class Questao7 {
+public class QuestaoSete {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int maior = 0, menor = 0, soma = 0;
@@ -22,24 +38,24 @@ public class Questao7 {
         for (int i = 0; i < 5; i++) {
             pessoas[i] = new Pessoa();
             System.out.print("Coloque o nome da pessoa: ");
-            pessoas[i].nome = sc.nextLine();
+            pessoas[i].setNome(sc.nextLine());
             System.out.print("Coloque a idade: ");
-            pessoas[i].idade = Integer.parseInt(sc.nextLine());
-            soma = soma + pessoas[i].idade;
+            pessoas[i].setIdade(Integer.parseInt(sc.nextLine()));
+            soma = soma + pessoas[i].getIdade();
             if (i == 0){
-                maior = pessoas[i].idade;
-                menor = pessoas[i].idade;
-                novo = pessoas[i].nome;
-                velho = pessoas[i].nome;
+                maior = pessoas[i].getIdade();
+                menor = pessoas[i].getIdade();
+                novo = pessoas[i].getNome();
+                velho = pessoas[i].getNome();
             }
             else {
-                if (maior < pessoas[i].idade) {
-                    maior = pessoas[i].idade;
-                    velho = pessoas[i].nome;
+                if (maior < pessoas[i].getIdade()) {
+                    maior = pessoas[i].getIdade();
+                    velho = pessoas[i].getNome();
                 }
-                if (menor > pessoas[i].idade) {
-                    menor = pessoas[i].idade;
-                    novo = pessoas[i].nome;
+                if (menor > pessoas[i].getIdade()) {
+                    menor = pessoas[i].getIdade();
+                    novo = pessoas[i].getNome();
                 }
             }
         }
