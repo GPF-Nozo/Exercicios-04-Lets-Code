375d88497a860cc06df4d0c2e0ed19a8fd5c2712
GPF-Nozo <90787054+GPF-Nozo@users.noreply.github.com>
2021-09-22 16:35:19 -0300

    Update QuestaoSete.java

--- QuestaoSete.java
+++ QuestaoSete.java
@@ -2,33 +2,6 @@ package br.com.letscode.java;
 
 import java.util.Scanner;
 
-class Pessoa {
-    private String nome;
-    private int idade;
-    public Pessoa() {
-
-    }
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-}
-
 public class QuestaoSete {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
