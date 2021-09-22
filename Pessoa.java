d17848d1307424470403222e26915ab9af0c8708
GPF-Nozo <90787054+GPF-Nozo@users.noreply.github.com>
2021-09-22 16:32:33 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,47 @@
+package br.com.letscode.java;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    public double alturaMetro;
+    public double pesoQuilograma;
+    public Pessoa() {
+
+    }
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setAltura(double alturaMetro) {
+        this.alturaMetro = alturaMetro;
+    }
+
+    public void setPeso(double pesoQuilograma) {
+        this.pesoQuilograma = pesoQuilograma;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getAltura() {
+        return alturaMetro;
+    }
+
+    public double getPeso() {
+        return pesoQuilograma;
+    }
+}
