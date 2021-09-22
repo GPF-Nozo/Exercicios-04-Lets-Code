30a931da007fdeebe1a73689e8eb1df7b0b8775c
GPF-Nozo <90787054+GPF-Nozo@users.noreply.github.com>
2021-09-22 16:21:56 -0300

    Update QuestaoOito.java

--- QuestaoOito.java
+++ QuestaoOito.java
@@ -3,17 +3,43 @@ package br.com.letscode.java;
 import java.util.Scanner;
 import java.lang.Math;
 
-class Pessoaa {
+class Pessoa {
     public String nome;
     public double alturaMetro;
     public double pesoQuilograma;
-    public Pessoaa(String nome, double alturaMetro, double pesoQuilograma) {
+    public Pessoa() {
+
+    }
+    public Pessoa(String nome, double alturaMetro, double pesoQuilograma) {
         this.nome = nome;
         this.alturaMetro = alturaMetro;
         this.pesoQuilograma = pesoQuilograma;
     }
-}
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAltura(double alturaMetro) {
+        this.alturaMetro = alturaMetro;
+    }
+
+    public void setPeso(double pesoQuilograma) {
+        this.pesoQuilograma = pesoQuilograma;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getAltura() {
+        return alturaMetro;
+    }
+
+    public double getPeso() {
+        return pesoQuilograma;
+    }
+}
 
 public class QuestaoOito {
     public static void main(String[] args){
@@ -21,18 +47,18 @@ public class QuestaoOito {
         Pessoaa[] pessoas = new Pessoaa[5];
         double imc;
         for(int i = 0; i < 5; i++){
+            pessoas[i] = new Pessoa();
             System.out.print("Coloque seu nome: ");
-            pessoas[i].nome = sc.nextLine();
+            pessoas[i].setNome(sc.nextLine());
             System.out.print("Coloque sua altura em metros: ");
-            pessoas[i].alturaMetro = Double.parseDouble(sc.nextLine());
+            pessoas[i].setAltura(Double.parseDouble(sc.nextLine()));
             System.out.print("Coloque seu peso em kg: ");
-            pessoas[i].pesoQuilograma = Double.parseDouble(sc.nextLine());
+            pessoas[i].setPeso(Double.parseDouble(sc.nextLine()));
         }
         for(int i = 0; i < 5; i++){
-            imc = pessoas[i].pesoQuilograma / (Math.pow(pessoas[i].alturaMetro, 2));
-            System.out.println(imc);
+            imc = pessoas[i].getPeso() / (Math.pow(pessoas[i].getAltura(), 2));
             if(imc < 18.5 || imc > 25) {
-                System.out.println("A pessoa " + pessoas[i].nome + " esta fora do peso ideal.");
+                System.out.println("A pessoa " + pessoas[i].getNome() + " esta fora do peso ideal, com imc de " + imc + ".");
             }
         }
         sc.close();
